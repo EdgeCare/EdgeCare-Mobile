2829a299e3456fe0cdce6e461e7888998e3007d9
Chinthani Chandeepa <119700964+Chinthanii@users.noreply.github.com>
2024-10-02 16:22:29 +0530

    Integrate ObjectBox for on device database

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,3 +3,16 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.jetbrains.kotlin.android) apply false
 }
+
+buildscript {
+    val objectboxVersion by extra("4.0.2")
+    repositories {
+        mavenCentral()
+
+        // Note: 2.9.0 and older are available on jcenter()
+    }
+    dependencies {
+        // Android Gradle Plugin 3.3.0 or later supported.
+        classpath("io.objectbox:objectbox-gradle-plugin:$objectboxVersion")
+    }
+}
