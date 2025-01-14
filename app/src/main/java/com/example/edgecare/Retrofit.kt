def8547dd5a823ec8cc617922593ee27e2a3573f
Thejan Weerasekara <92569392+ThejanB@users.noreply.github.com>
2025-01-15 04:47:10 +0530

    API request time limit increased to 3 minutes

--- app/src/main/java/com/example/edgecare/Retrofit.kt
+++ app/src/main/java/com/example/edgecare/Retrofit.kt
@@ -1,11 +1,20 @@
 package com.example.edgecare
 
 import com.example.edgecare.api.ApiService
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+
+private val okHttpClient = OkHttpClient.Builder()
+    .connectTimeout(3, TimeUnit.MINUTES)
+    .readTimeout(3, TimeUnit.MINUTES)
+    .writeTimeout(3, TimeUnit.MINUTES)
+    .build()
 
 val retrofit = Retrofit.Builder()
     .baseUrl("http://10.15.94.116:8000/")
+    .client(okHttpClient)
     .addConverterFactory(GsonConverterFactory.create()) // Use Gson for JSON parsing
     .build()
 
